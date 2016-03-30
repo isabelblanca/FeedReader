8dbc670f363950c68c662b28a6550d86d11e038f
isabelblanco <isabelblancodura@gmail.com>
2016-03-30 18:10:01 +0100

    Steps 9-10. Test is the menu is hidden by default

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -9,7 +9,6 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-    console.log(allFeeds[1].url)
     /* This is our first test suite - a test suite just contains
     * a related set of tests. This suite is all about the RSS
     * feeds definitions, the allFeeds variable in our application.
@@ -50,21 +49,23 @@ $(function() {
         }
     });
 
-
+    console.log ($("body").attr("class"))
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('the menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-
+        it ('menu hidden by default', function() {
+            expect($("body").attr("class")).toEqual("menu-hidden");
+        })
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-
+    });
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
@@ -73,7 +74,6 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-
     /* TODO: Write a new test suite named "New Feed Selection"
 
         /* TODO: Write a test that ensures when a new feed is loaded
